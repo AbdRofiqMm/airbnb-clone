c915de3f6f5b4e4dda7470f7db1569b3e1183d66
AbdRofiqMm <88978157+AbdRofiqMm@users.noreply.github.com>
2024-02-02 14:08:28 +0800

    fix: Auth form Validations

--- components/auth/SignupModal.tsx
+++ components/auth/SignupModal.tsx
@@ -6,9 +6,21 @@ import { Label } from "../ui/label";
 import { Input } from "../ui/input";
 import { Button } from "../ui/button";
 import Image from "next/image";
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import { RegisterType, registerSchema } from "@/validations/authSchema";
 
 export default function SignupModal() {
   const [open, setOpen] = useState<boolean>(false);
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<RegisterType>({
+    resolver: yupResolver(registerSchema),
+  });
+  const onSubmit = (data: RegisterType) => console.log(data);
+
   return (
     <AlertDialog open={open}>
       <AlertDialogTrigger asChild>
@@ -31,39 +43,43 @@ export default function SignupModal() {
             </div>
           </AlertDialogTitle>
           <AlertDialogDescription>
-            <form action="">
+            <form onSubmit={handleSubmit(onSubmit)}>
               <h1 className="text-lg font-bold">Welcome to Airbnb</h1>
               <div className="mt-5">
                 <Label htmlFor="name">Name</Label>
                 <Input
                   placeholder="Enter your email"
                   id="name"
+                  {...register("name")}
                 />
-                <span className="text-red-400"></span>
+                <span className="text-red-400">{errors?.name?.message}</span>
               </div>
               <div className="mt-5">
                 <Label htmlFor="email">Email</Label>
                 <Input
                   placeholder="Enter your email"
                   id="email"
+                  {...register("email")}
                 />
-                <span className="text-red-400"></span>
+                <span className="text-red-400">{errors?.email?.message}</span>
               </div>
               <div className="mt-5">
                 <Label htmlFor="password">Password</Label>
                 <Input
                   placeholder="Enter your password"
                   id="password"
+                  {...register("password")}
                 />
-                <span className="text-red-400"></span>
+                <span className="text-red-400">{errors?.password?.message}</span>
               </div>
               <div className="mt-5">
                 <Label htmlFor="cpassword">Confirm Password</Label>
                 <Input
                   placeholder="Confirm your password"
                   id="cpassword"
+                  {...register("password_confirmation")}
                 />
-                <span className="text-red-400"></span>
+                <span className="text-red-400">{errors?.password_confirmation?.message}</span>
               </div>
               <div className="mt-5">
                 <Button className="bg-brand w-full">Continue</Button>
